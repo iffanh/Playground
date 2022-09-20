3a32ccbeec793a6f7b9470ba3126181c8c06e11d
Muhammad Iffan Hannanu <iffan.hannanu@gmail.com>
2022-09-20 10:16:41 +0200

    [Optimization] Finish lagrange interpolation

--- Optimization/Polynomials/lagrange_polynomial.py
+++ Optimization/Polynomials/lagrange_polynomial.py
@@ -14,9 +14,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import itertools
 import scipy.special
-from typing import Tuple
+from typing import Tuple, List
 import sympy as sp
 from sympy import lambdify
+
+class PolynomialBase:
+    def __init__(self, symbol, feval:callable) -> None:
+        
+        self.symbol = symbol
+        self.feval = feval
+        
+class LagrangePolynomial:
+    def __init__(self, symbol, feval:callable) -> None:
+        
+        self.symbol = symbol
+        self.feval = feval
+        
+class ModelPolynomial:
+    def __init__(self, symbol, feval:callable) -> None:
+        
+        self.symbol = symbol
+        self.feval = feval
+
 
 class LagrangePolynomials:
     def __init__(self, v:np.ndarray, f:np.ndarray, pdegree:int = 2):
@@ -31,6 +50,12 @@ class LagrangePolynomials:
         Methods:
             .interpolate : Given a vector x it will interpolate using the polynomials constructed 
                            by this class
+                           
+        Attributes: 
+            .lagrange_polynomials : All the lagrange polynomials, lambda_i(x), wrapped in LagrangePolynomial class
+            .model_polynomial : Ultimate polynomial model for the given interpolation set, wrpaped in ModelPolynomial class
+            .polynomial_basis : List of the polynomial basis, wrapped in PolynomialBase class
+        
         """
 
         self.f = f
@@ -39,100 +64,121 @@ class LagrangePolynomials:
         
         self.multiindices = self._get_multiindices(self.N, pdegree, self.P)
         self.coefficients = self._get_coefficients(self.multiindices)
-        self.polynomial_basis = self._get_polynomial_basis(self.multiindices, self.coefficients)
-        # self.det, self.matrix = self._build_matrix(v, self.multiindices, self.coefficients)
-        # self.alpha = self._solve_for_alpha(self.matrix, f)
-        # self._check_interpolation_quality(v, f)
+        self.polynomial_basis, self.input_symbols = self._get_polynomial_basis(self.multiindices, self.coefficients)
+        self.lagrange_polynomials = self._build_lagrange_polynomials(self.polynomial_basis, v, self.input_symbols)
+        self.model_polynomial = self._build_model_polynomial(self.lagrange_polynomials, f, self.input_symbols)
         
     def interpolate(self, x:np.ndarray) -> float:
-        """Given the polynomial construction when this class is initialized, it will return the interpolated values
+        """Interpolate using the interpolation model given the input x. 
+           Maps R^n -> R, where n is the dimension of input data
 
         Args:
-            x (np.ndarray): input vector
-
-        Raises:
-            Exception: if the dimension doesn't match
+            x (np.ndarray): Input data, R^n
 
         Returns:
-            float: interpolated value
+            float: Interpolation value
+        """
+        return self.model_polynomial.feval(*x)
+        
+    def _build_model_polynomial(self, lagrange_polynomials:List[LagrangePolynomial], f:np.ndarray, input_symbols:list) -> ModelPolynomial:
+        """ Generate model polynomial, m(x), given the lagrange polynomials and the interpolation set value
+
+            m(x) = sum(lambda_i(x)*f(y_i))
+            
+        Args:
+            lagrange_polynomials (List[LagrangePolynomial]): List of lagrange polynomials
+            f (np.ndarray): interpolation set value
+            input_symbols (list): _description_
+
+        Returns:
+            ModelPolynomial: Model polynomial
         """
         
-        if x.shape[0] != len(self.multiindices[0]):
-            raise Exception(f"Dimension of x : {x.shape} is incompatible with the dimension to the basis input : {len(self.multiindices[0])}")
+        polynomial_sum = 0 
+        for i in range(len(lagrange_polynomials)):
+            polynomial_sum += lagrange_polynomials[i].symbol*f[i]
+            
+        return ModelPolynomial(polynomial_sum, lambdify(input_symbols, polynomial_sum, 'numpy'))
+
+    def _build_lagrange_polynomials(self, basis:List[PolynomialBase], data_points:np.ndarray, input_symbols:list) -> List[LagrangePolynomial]:
+        """ Responsible for generating the lagrange polynomials using Cramer's rule: 
+            lambda(x) = det(M(Y_i, \phi))/det(M(Y, \phi))
+            
+            where Y_i = Y \ y_i \cup x
+
+        Args:
+            basis (List[PolynomialBase]): List of polynomial base
+            data_points (np.ndarray): matrix of interpolation set input Y {y1, y2, ..., yp}
+            input_symbols (list): list of sympy symbol for the input X = {x1, x2, ..., xn}
+
+        Returns:
+            List[LagrangePolynomial]: Lagrange polynomials, {lambda_0, lambda_1, ..., lambda_p}
+        """
         
-        result = 0
-        for j, alpha in enumerate(self.alpha):
-            result += alpha*self._get_basis_func_eval(x, self.multiindices[j], self.coefficients[j])
+        # Create the full matrix
+        matrix = []
+        for i in range(data_points.shape[1]):
+            input = data_points[:,i]
+            entry = [d.feval(*input) for d in basis]
+            matrix.append(entry)
+        self.full_matrix = sp.Matrix(matrix)
+        full_det = self.full_matrix.det()
         
-        return result
+        # create the var matrix: matrix in which for each set i in the interpolation set, 
+        # create another set of Y_i = Y \ y_i \cup x
+        self.var_matrices = []
+        lpolynomials = []
+        for i in range(data_points.shape[1]):
+            var_matrix = matrix[:]
+            var_matrix[i] = [d.feval(*input_symbols) for d in basis]
+            var_matrix = sp.Matrix(var_matrix)
+            self.var_matrices.append(var_matrix)
+            
+            lpolynomial = var_matrix.det()/full_det
+            lpolynomials.append(LagrangePolynomial(lpolynomial, lambdify(input_symbols, lpolynomial, 'numpy')))
         
-    # def _create_
+        print(f"Lagrange polynomials are: {[p.symbol for p in lpolynomials]}")
+        
+        return lpolynomials
     
-    def _get_polynomial_basis(self, exponents:list, coefficients:list): # -> list(Tuple[callable]):
+    def _get_polynomial_basis(self, exponents:list, coefficients:list) -> Tuple[List[PolynomialBase], list]:
+        """Responsible for generating all the polynomial basis, also returning input_symbols
+
+        Args:
+            exponents (list): combination of all possible exponents
+            coefficients (list): combination of all possible coefficients
+
+        Returns:
+            Tuple[List[PolynomialBase], list]: List of polynomial bases and input symbols
+        """
 
         Ndim = len(exponents[0])
         print(f"Dimension length, N = {Ndim}")
-        self.symbols = [sp.symbols('x%s'%(ind+1)) for ind in range(Ndim)]
-        print(f"Symbols = {self.symbols}")
-        self.basis = []
+        input_symbols = [sp.symbols('x%s'%(ind+1)) for ind in range(Ndim)]
+        print(f"Symbols = {input_symbols}")
+        basis = []
         for (exps, coef) in zip(exponents, coefficients):
             b = 1
-            for symb, exp in zip(self.symbols, exps):
+            for symb, exp in zip(input_symbols, exps):
                 b *= symb**exp
             b = b/coef
             
             # (phi(x) sympy symbol, phi(x) evaluation), function evaluation called using a numpy array x, phi(*x)
-            self.basis.append((b, lambdify(self.symbols, b, 'numpy'))) 
+            basis.append(PolynomialBase(b, lambdify(input_symbols, b, 'numpy'))) 
 
-        print(f"The polynomial basis are: {self.basis}")
+        print(f"The polynomial basis (phi) are: {[d.symbol for d in basis]}")
 
-        return self.basis
+        return basis, input_symbols
 
-    def node_specific_constant(self, ind:int, v:np.ndarray) -> float: 
-
-        return 
-    
-    def _build_matrix(self, v:np.ndarray, multiindices:list, coefficients:list) -> Tuple[float, np.ndarray]:
-        """This function is responsible for building the M matrix in M \alpha = f
-
-        Args:
-            v (np.ndarray): array of interpolation data points
-            multiindices (list): list of indices to be used as exponents. The length is equal to the dimension of the data 
-            coefficients (list): list of coefficients. The length is equal to the dimension of the data
-
-        Raises:
-            Exception: When the constructed matrix is singular
-
-        Returns:
-            Tuple[float, np.ndarray]: the determinant and the matrix M
-        """
-        
-        
-        matrix = np.zeros((v.shape[1], len(coefficients)))
-        for i in range(v.shape[1]): # number of data points
-            for j in range(len(coefficients)): # number of possible combination of basis
-                matrix[i,j] = self._get_basis_func_eval(v[:,i], multiindices[j], coefficients[j])
-        
-        det = np.linalg.det(matrix)
-        if det == 0.0:
-            raise Exception("The matrix M is singular")
-        
-        return det, matrix
-
-    def _get_basis_func_eval(self, x:np.ndarray, index:list, coeff:float) -> float:
-        """ Evaluate the basis function \phi_i(x). Maps R^n -> R
-
-        Args:
-            x (np.ndarray): input vector 
-            index (list): indices of the exponent, obtained from get_multiindices
-            coeff (float): "coefficient" of the basis, obtained from get_coefficients
-
-        Returns:
-            float : basis evaluation
-        """
-        return np.multiply(*np.power(x, index))/coeff
-    
     def _get_coefficients(self, multiindices:list) -> list:
+        """Generate coefficients for every possible polynomial basis
+
+        Args:
+            multiindices (list): list of indices that will be used as the reciprocal in the coefficient calculation
+
+        Returns:
+            list: all possible combination of the coefficients
+        """
         coefficients = [np.multiply(*scipy.special.factorial(ind)) for ind in multiindices]
         return coefficients
     
@@ -154,6 +200,8 @@ class LagrangePolynomials:
         
         if self.P < len(multiindices):
             raise Exception(f"The length of interpolation points: {p} is less than the minimum requirement : {len(multiindices)}")
+        elif self.P > len(multiindices):
+            raise Exception(f"The length of interpolation points: {p} is greater than the minimum requirement : {len(multiindices)}")
         
         return multiindices 
          
