f0a1baabdffb57f8af7e76b7026c2b40e7091d69
Muhammad Iffan Hannanu <iffan.hannanu@gmail.com>
2022-12-16 16:46:56 +0100

    [TR] fix filters

--- Optimization/Trust-region/src/utils/filter.py
+++ Optimization/Trust-region/src/utils/filter.py
@@ -8,23 +8,41 @@ class FilterSQP():
         
         self.constants = constants
     
-    def add_to_filter(self, coordinate:Tuple[float, float]):
+    def add_to_filter(self, coordinate:Tuple[float, float]) -> bool:
+        # coordinate is a combination of (OF, violation)
         
-        for i, coord in enumerate(self.filters):
-                 
-            if coordinate[1] < (1 - self.constants['gamma_vartheta'])*coord[1] or coordinate[0] < coord[0] - self.constants['gamma_vartheta']*coordinate[1]: 
-                self.filters.append(coordinate)
-                return True
-            
-            if coordinate[1] < (1 - self.constants['gamma_vartheta'])*coord[1] and coordinate[0] < coord[0] - self.constants['gamma_vartheta']*coordinate[1]: 
-                self.filters[i] = coordinate
-                return True
-            
-            if coordinate[1] >= (1 - self.constants['gamma_vartheta'])*coord[1] and coordinate[0] >= coord[0] - self.constants['gamma_vartheta']*coordinate[1]:
-                return False
-            
-        self.filters.append(coordinate)
+        print(self.filters)
         self.filters.sort(key=lambda x: x[1])
+        if len(self.filters) == 0:
+            self.filters.append(coordinate)
+            return True
         
-        return True
+        is_acceptable = False
+            
+        if coordinate[1] < (1 - self.constants['gamma_vartheta'])*self.filters[0][1] and coordinate[0] > self.filters[0][0] - self.constants['gamma_vartheta']*coordinate[1]:
+            self.filters.append(coordinate)
+            is_acceptable = True
+        
+        elif coordinate[1] > (1 - self.constants['gamma_vartheta'])*self.filters[-1][1] and coordinate[0] < self.filters[-1][0] - self.constants['gamma_vartheta']*coordinate[1]:
+            self.filters.append(coordinate)
+            is_acceptable = True
+        
+        else:
+            ## Check main pareto front                
+            curr_filter = []
+            does_exist = False
+            for i, coord in enumerate(self.filters):
+                if coordinate[1] < (1 - self.constants['gamma_vartheta'])*coord[1] and coordinate[0] < coord[0] - self.constants['gamma_vartheta']*coordinate[1]:
+                    curr_filter.append(coordinate)
+                    is_acceptable = True
+                    does_exist = True
+                else:
+                    if does_exist:
+                        pass
+                    else:
+                        curr_filter.append(coord)
+                
+            self.filters = curr_filter
+        
+        return is_acceptable
         
